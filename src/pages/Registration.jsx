f4a634d9c7a12b7a6a395bfc9f0cca6fa531e12b
pvsaint <vpeter817@gmail.com>
2024-11-14 18:18:20 +0100

    solved conflict

--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -9,13 +9,29 @@ import { MdOutlineClose } from "react-icons/md";
 import { RxHamburgerMenu } from "react-icons/rx";
 import bg from "../assets/back.jpeg";
 import axios from "axios";
-import { useState } from "react";
 import { Toaster, toast } from "react-hot-toast";
 import { CiUser } from "react-icons/ci";
 import { MdOutlineEmail } from "react-icons/md";
 import { CiPhone } from "react-icons/ci";
 
 function Registration() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isScrolled, setIsScrolled] = useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   const [formData, setFormData] = useState({
     track: "",
     email: "",
@@ -76,7 +92,56 @@ function Registration() {
   };
   return (
     <div className="min-h-screen flex flex-col">
-      <NavBar />
+      <NavBar
+        className={`w-full fixed top-0 left-0 z-50 ${
+          isScrolled ? "bg-[#efefef]" : "bg-transparent"
+        } transition-colors duration-500`}
+      >
+        <div className="md:flex items-center justify-between py-4 px-7 z-50">
+          <div className="font-bold text-2xl cursor-pointer flex items-center">
+            <Link to="/">
+              <img className="w-[150px]" src={logo} alt="Community logo" />
+            </Link>
+          </div>
+          <button
+            className="text-3xl absolute right-8 top-6 cursor-pointer md:hidden"
+            onClick={() => setIsMenuOpen((e) => !e)}
+          >
+            {isMenuOpen ? <MdOutlineClose /> : <RxHamburgerMenu />}
+          </button>
+          <ul
+            className={`md:flex md:items-center md:pb-5 pb-10 absolute md:static bg-[#e5e5e5] md:bg-transparent md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 p-5 transition-all duration-500 ease-in ${
+              isMenuOpen ? "top-0 opacity-100 pt-14" : "top-[-490px]"
+            }`}
+          >
+            {/* Menu items */}
+            <li className="md:ml-8 text-xl md:my-0 my-7 hover:text-gray-400 duration-500">
+              <Link to="/">Home</Link>
+            </li>
+            <li className="md:ml-8 text-xl md:my-0 my-7 hover:text-gray-400 duration-500">
+              <Link to="/about">About</Link>
+            </li>
+            <li className="md:ml-8 text-xl md:my-0 my-7 hover:text-gray-400 duration-500">
+              <Link to="/training">Trainings</Link>
+            </li>
+            <li className="md:ml-8 text-xl md:my-0 my-7 hover:text-gray-400 duration-500">
+              <Link to="/events">Events</Link>
+            </li>
+            <li className="md:ml-8 text-xl md:my-0 my-7 hover:text-gray-400 duration-500">
+              <Link to="/blog">Blogs</Link>
+            </li>
+
+            <span className="flex flex-col md:flex-row gap-5 lg:ml-[130px]">
+              <Button className="bg-[#1E90FF] p-3 rounded-lg md:ml-8 hover:bg-gray-500 duration-500 text-[white]">
+                Join Us
+              </Button>
+              <Button className="bg-none px-5 py-3 border-2 border-white rounded-lg hover:bg-gray-500 hover:text-white duration-500">
+                Contact Us
+              </Button>
+            </span>
+          </ul>
+        </div>
+      </NavBar>
 
       <main
         className="flex-1 w-full mt-[70px] md:mt-[100px] lg:mt-[150px] overflow-x-hidden "
@@ -94,7 +159,7 @@ function Registration() {
             </h2>
           </div>
         </section>
-        <section className=" flex flex-col justify-center overflow-hidden  ">
+        <section className="mt-20 flex flex-col justify-center overflow-hidden  ">
           {/* Registration Form */}
           <div
             className=""
